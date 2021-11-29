9f5559132e0130c09d06151b836bb7d25cbbe693
anshul <anshul.bansod2@gmail.com>
2021-11-29 11:11:22 +0530

    added allowed host

--- carZone/settings.py
+++ carZone/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-g$v(x!f+@zak^c*sjki0i4-&+5djo&2@dd102(mc@7f98w#qtg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['floating-hollows-07584.herokuapp.com',]
 
 LOGIN_REDIRECT_URL = 'dashboard'
 
